adb06b7500e849a154fdbb446d8be2302c1b426c
ferigalung <ferigalungcom@gmail.com>
2022-12-22 10:41:21 +0700

    add some frontend routing

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 require('colors');
 
+const path = require('path');
 const express = require('express');
 const port = process.env.PORT || 5000;
 const errorHandler = require('./helpers/middlewares/errorHandler');
@@ -16,6 +17,16 @@ app.use(express.urlencoded({extended: false}));
 //routes
 app.use('/api/goals/v1', require('./routes/goalRoutes'));
 app.use('/api/users/v1', require('./routes/userRoutes'));
+
+if(process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
+	});
+} else {
+	app.get('/', (req, res) => res.send('App still in development environment'));
+}
 
 app.use(errorHandler);
 app.listen(port, () => {
